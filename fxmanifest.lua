930e24180f55cb6f852ab18a2c484e180d653995
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-05-26 21:29:21 +0200

    fix(fxmanifest): oxmysql fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,14 +4,14 @@ description 'A clean NUI bank system'
 version '1.0.0' 
 author 'NickBlade#5623' 
 shared_scripts { 
-   './shared/config.lua' 
+   'shared/config.lua' 
 } 
 server_scripts { 
-	'./server/client.lua',
-	'./server/modules/framework.lua' 
+	'@oxmysql/lib/MySQL.lua',
+	'server/client.lua',
+	'server/modules/framework.lua' 
 } 
 client_scripts { 
-	'./client/client.lua', 
-	'./client/modules/framework.lua',
-	'@oxmysql/lib/MySQL.lua'
+	'client/client.lua', 
+	'client/modules/framework.lua'
 } 
